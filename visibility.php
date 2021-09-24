74c0dcaf8492731fb26af4dc6ac5cc3ee1f17c3c
rifky86 <goblogpayah123@gmail.com>
2021-09-24 22:32:23 +0700

    visbility (tugas 3)

--- visibility.php
+++ visibility.php
@@ -9,8 +9,9 @@ class Produk {
     //Property
     public  $judul,
             $penulis,
-            $penerbit,
-            $harga;
+            $penerbit;
+    protected $diskon = 0;
+    private $harga;
 
     // Construktor
     public function __construct($judul = "judul",$penulis = "penulis",$penerbit = "penerbit",$harga = 0)
@@ -20,7 +21,13 @@ class Produk {
         $this->penerbit = $penerbit;
         $this->harga = $harga;
     }
+
     // method
+    
+    public function getHarga()
+    {
+        return $this->harga - ($this->harga * $this->diskon / 100);
+    }
     public function getLebel(){
         return "$this->penulis,$this->penerbit";
     }
@@ -55,11 +62,16 @@ class Komik extends Produk {
 class Game extends Produk {
 
     public $waktuMain;
-
-    public function __construct($judul = "judul",$penulis = "penulis",$penerbit = "penerbit",$harga = 0, $waktuMain = 0)
+    //method
+    public function __construct($judul = "judul",$penulis = "penulis",$penerbit = "penerbit", $harga = 0, $waktuMain = 0)
     {
         parent::__construct($judul,$penulis,$penerbit,$harga);
         $this->waktuMain = $waktuMain;
+    }
+
+    public function setDiskon( $diskon )
+    {
+        $this->diskon = $diskon;
     }
 
 
@@ -68,6 +80,7 @@ class Game extends Produk {
 
         return $str;
     }
+
 }
 
 class CetakInfoProduk{
@@ -84,3 +97,6 @@ $produk2 = new Game("Uncharted", "Nail Drickmann", "Sony Computer", "250000", "5
 echo $produk1->getInfoProduk();
 echo "<br>";
 echo $produk2->getInfoProduk();
+echo "<hr>";
+$produk2->setDiskon(50);
+echo $produk2->getHarga();
